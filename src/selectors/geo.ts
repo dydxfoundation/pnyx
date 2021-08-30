cd73ff6de528069c6d8a6a48e124a2df21233dc0
Everett Hu <everettwho@gmail.com>
2021-08-30 12:42:41 -0700

    Cleanup

--- src/selectors/geo.ts
+++ src/selectors/geo.ts
@@ -7,7 +7,5 @@ import { BLOCKED_COUNTRIES } from 'constants/geo';
 
 export const getGeoData = (state: RootState): GeoData | null => state.geo.geoData;
 
-// export const getIsUserGeoBlocked = (state: RootState): boolean =>
-//   _.includes(BLOCKED_COUNTRIES, state.geo.geoData?.country);
-
-export const getIsUserGeoBlocked = (state: RootState): boolean => false;
+export const getIsUserGeoBlocked = (state: RootState): boolean =>
+  _.includes(BLOCKED_COUNTRIES, state.geo.geoData?.country);
