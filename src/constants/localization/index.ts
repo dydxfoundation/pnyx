7b057748fedc0242064f8e9bbddc6b5798c29caf
Darryl Yeo <dev@darryl-yeo.com>
2022-05-24 13:22:09 -0700

    Add Turkish, French, Latam Spanish and Brazilian Portuguese (#38)
    * Add Turkish, French, Latam Spanish and Brazilian Portuguese
    
    * Update order

--- src/constants/localization/index.ts
+++ src/constants/localization/index.ts
@@ -4,21 +4,21 @@ import { APP_STRING_KEYS } from './app';
 import { ERROR_STRING_KEYS } from './errors';
 import { WARNING_STRING_KEYS } from './warnings';
 
-export const SUPPORTED_LOCALE_BASE_TAGS = {
+export const SUPPORTED_LOCALE_BASE_TAGS: Record<SupportedLocale, string> = {
   [SupportedLocale.EN]: 'en',
   [SupportedLocale.ZH_CN]: 'zh',
   [SupportedLocale.JA]: 'ja',
   [SupportedLocale.KO]: 'ko',
   [SupportedLocale.RU]: 'ru',
+  [SupportedLocale.TR]: 'tr',
+  [SupportedLocale.FR]: 'fr',
+  [SupportedLocale.PT]: 'pt',
+  [SupportedLocale.ES]: 'es',
 };
 
-export const SUPPORTED_BASE_TAGS_LOCALE_MAPPING: { [key: string]: SupportedLocale } = {
-  en: SupportedLocale.EN,
-  zh: SupportedLocale.ZH_CN,
-  ja: SupportedLocale.JA,
-  ko: SupportedLocale.KO,
-  ru: SupportedLocale.RU,
-};
+export const SUPPORTED_BASE_TAGS_LOCALE_MAPPING = Object.fromEntries(
+  Object.entries(SUPPORTED_LOCALE_BASE_TAGS).map(([locale, baseTag]) => [baseTag, locale as SupportedLocale])
+);
 
 export const SUPPORTED_LOCALE_STRING_LABELS = {
   [SupportedLocale.EN]: 'English',
@@ -26,6 +26,10 @@ export const SUPPORTED_LOCALE_STRING_LABELS = {
   [SupportedLocale.JA]: '日本語',
   [SupportedLocale.KO]: '한국어',
   [SupportedLocale.RU]: 'русский',
+  [SupportedLocale.TR]: 'Türkçe',
+  [SupportedLocale.FR]: 'Français',
+  [SupportedLocale.PT]: 'Português',
+  [SupportedLocale.ES]: 'Español',
 };
 
 export const STRING_KEYS = {
