09d34d5cef849d9cc32ed6cd3fd7f8b186f796b2
Darryl Yeo <dev@darryl-yeo.com>
2023-04-05 17:12:46 -0700

    vite: export .svg files as React components using vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+import svgr from 'vite-plugin-svgr';
 import nodePolyfills from 'vite-plugin-node-stdlib-browser'
+
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    svgr({
+      exportAsDefault: true,
+    }),
     nodePolyfills(),
   ],
   resolve: {
